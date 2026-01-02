de58b77d8c05db510d9c9c8331b314a9d26fc58d
FADED <mahletgetinet17@gmail.com>
2026-01-02 23:12:04 +0300

    Add initial union implementation with example usage

---
+++ union.cpp
@@ -0,0 +1,25 @@
+/*                      UNION                                
+    - A Union is a user-defined data type (like struct)
+    - All memberes share the same memory location
+    - Only one member can store a valid value at a time
+    -  
+    
+              union union_name {
+              data_type member1;
+              data_type member2;
+              ...
+                };
+
+    - Size of the largest member.
+        
+        union MyUnion {
+        int i;
+        float f;
+        char c;
+                 };   
+    - now the size of int=4. float=4. char=1;
+    since the largest size is 4,the size of the union.
+
+
+*/
+#
