0e4452a356a63e3b4b54fb9054e7cb6829dc5173
FADED <mahletgetinet17@gmail.com>
2026-01-02 23:50:38 +0300

    union practice

--- union.cpp
+++ union.cpp
@@ -34,29 +34,28 @@
 using namespace std;
 
 union Data {
-    int four;
-    float quatro;
-    char one;
+    int i;
+    float f;
+    char c;
 };
-int main (){
-    Data d;
-    d.four=21;
-    cout<<"The value of Four is"<<d.four<<endl;
 
-    return 0;
+
+/*str[20]*/
+
+int main(){
+     Data d;
+     d.i=12;
+     cout<<"i== "<<d.i<<endl;
+
+    d.f=12.32;
+    cout<<"f== "<<d.f<<endl;
+  return 0;
+
+/*Explain why the previously stored values become invalid.*/
+/*Here is why; if we try to print the value of the 'i' the terminal will give us a
+garbage output, because the value has been overwritten. Since Union works in a way that secures 
+one memory for the whole member, once that memory is occupied, only that value will be valid THEN.*/
+
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 /*Only one member is valid at a time*/
