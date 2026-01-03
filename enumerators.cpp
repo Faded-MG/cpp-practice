792dc94f54ce0babfb0a99aac382f8942d85a035
FADED <mahletgetinet17@gmail.com>
2026-01-03 22:39:43 +0300

    Add initial implementation of enumerators with Color enum and main function

---
+++ enumerators.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+
+
+enum Color {
+    RED,    
+    GREEN,  
+    BLUE    
+};
+
+int main() {
+    Color c = GREEN;  
+
+    if (c == GREEN) {
+        cout << "Color is green";
+    }
+
+
+
+
+return 0;
+}
