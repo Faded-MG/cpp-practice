a4ec10ec302ca658c7954017e8503027163b4040
FADED <mahletgetinet17@gmail.com>
2026-01-06 23:56:06 +0300

    Implement student record system using structs, unions, and pointers

---
+++ revise2.cpp
@@ -0,0 +1,52 @@
+#include <iostream>
+#include <cstring>
+using namespace std;
+enum ProgramType {REGULAR, EXTENSION, DISTANCE};
+
+union ContactInfo {
+    char email[30];
+    long long phoneNumber;
+
+};
+struct PersonalInfo {
+    char name[30];
+    int age;
+    ContactInfo contact;
+
+};
+struct Student {
+    int id;
+    ProgramType program;
+    PersonalInfo info;
+    float cgpa;
+};
+int main (){
+    Student s;
+    Student * ptr =&s;
+  
+    ptr->cgpa=3.80;
+    ptr->id=5554;
+    ptr->info.age=20;
+   
+
+    strcpy(ptr->info.name, "Mahlet");  
+    ptr->info.age = 20;
+
+
+    ptr->info.contact.phoneNumber = 1234567890;
+   
+
+cout<<"=======STUDENT RECOD MANAGMENT SYSTEM======"<<endl;
+cout<<"ID"<<ptr->id<<endl;
+
+    cout << "Name: " << ptr->info.name << endl;
+    cout << "Age: " << ptr->info.age << endl;
+    cout << "Program: "
+         << (ptr->program == REGULAR ? "REGULAR" :
+             ptr->program == EXTENSION ? "EXTENSION" : "DISTANCE")
+         << endl;
+    cout << "Contact: " << ptr->info.contact.phoneNumber << endl;
+    cout << "CGPA: " << ptr->cgpa << endl;
+
+    return 0;
+}
